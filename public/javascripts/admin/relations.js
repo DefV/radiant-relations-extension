f53ef34e1eea4e1898d42c205f0d8684f0688111
Jan De Poorter <jan@defv.be>
2011-06-28 23:12:41 +0200

    Update JS to working version

--- public/javascripts/admin/relations.js
+++ public/javascripts/admin/relations.js
@@ -1,7 +1,17 @@
 document.observe('click', function(e) {
- var remove;
- if (remove = e.findElement('.relation .remove-row')) {
-   if ()
-   remove.up('selector-row').remove();
+  var remove, link;
+  
+  if (remove = e.findElement('.remove-row')) {
+    if ($$('.selector-row').length > 1)
+      remove.up('.selector-row').remove();
+    else
+      remove.up('.selector-row').select('select').first().value = '';
+ }
+ 
+ if (e.findElement('.add-row')) {
+   var new_row = $$('.selector-row').last().cloneNode(true);
+   $$('.selector-row').last().insert({after: new_row})
+   
+   return false;
  }
 })
